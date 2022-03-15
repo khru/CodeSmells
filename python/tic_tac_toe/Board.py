afde575290c1fd717c0fb9b68784673552092adf
Marc Mauri Alloza <marc.mauri@codurance.com>
2022-03-15 17:21:24 +0100

    create private constant with board size

--- python/tic_tac_toe/Board.py
+++ python/tic_tac_toe/Board.py
@@ -2,11 +2,11 @@ from tic_tac_toe.Tile import Tile
 
 
 class Board(object):
-
+    __BOARD_SIZE = 3
     def __init__(self):
         self._plays = []
-        for i in range(3):
-            for j in range(3):
+        for i in range(self.__BOARD_SIZE):
+            for j in range(self.__BOARD_SIZE):
                 tile = Tile()
                 tile.X = i
                 tile.Y = j
