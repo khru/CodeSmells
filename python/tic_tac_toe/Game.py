e48b568f0459435e93396a6d70189c0938065b5a
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-03-15 17:47:05 +0100

    refactor: rename last_player and change equality

--- python/tic_tac_toe/Game.py
+++ python/tic_tac_toe/Game.py
@@ -4,50 +4,47 @@ from tic_tac_toe.Board import Board
 class Game(object):
 
     def __init__(self):
-        self._lastSymbol = None
+        self._last_player = None
         self._board = Board()
 
     def play(self, symbol, x, y):
         # if first move
-        if self._lastSymbol == None:
-            # if player is X
-            if symbol == 'O':
-                raise Exception('Invalid first player')
-        # if not first move but player repeated
-        elif symbol == self._lastSymbol:
-            raise Exception('Invalid next player')
-        # if not first move but play on an already played tile
-        elif self._board.tile_at(x, y) != None:
+        if self._last_player is None and symbol == 'O':
+            raise Exception('Invalid first player')
 
+        if symbol == self._last_player:
+            raise Exception('Invalid next player')
+
+        if self._board.tile_at(x, y) is not None:
             raise Exception('Invalid position')
 
         # update game state
-        self._lastSymbol = symbol
+        self._last_player = symbol
         self._board.add_tile_at(symbol, x, y)
 
     def winner(self):
         # if the positions in first row are taken
-        if self._board.tile_at(0, 0).Symbol != None \
-                and self._board.tile_at(0, 1) != None \
-                and self._board.tile_at(0, 2) != None:
+        if self._board.tile_at(0, 0).Symbol is not None \
+                and self._board.tile_at(0, 1) is not None \
+                and self._board.tile_at(0, 2) is not None:
             # if first row is full with same symbol
             if self._board.tile_at(0, 0).Symbol == self._board.tile_at(0, 1).Symbol \
                     and self._board.tile_at(0, 2).Symbol == self._board.tile_at(0, 1).Symbol:
                 return self._board.tile_at(0, 0).Symbol
 
         # if the positions in first row are taken
-        if self._board.tile_at(1, 0) != None \
-                and self._board.tile_at(1, 1) != None \
-                and self._board.tile_at(1, 2) != None:
+        if self._board.tile_at(1, 0) is not None \
+                and self._board.tile_at(1, 1) is not None \
+                and self._board.tile_at(1, 2) is not None:
             # if first row is full with same symbol
             if self._board.tile_at(1, 0).Symbol == self._board.tile_at(1, 1).Symbol \
                     and self._board.tile_at(1, 2).Symbol == self._board.tile_at(1, 1).Symbol:
                 return self._board.tile_at(1, 0).Symbol
 
         # if the positions in first row are taken
-        if self._board.tile_at(2, 0) != None \
-                and self._board.tile_at(2, 1) != None \
-                and self._board.tile_at(2, 2).Symbol != None:
+        if self._board.tile_at(2, 0) is not None \
+                and self._board.tile_at(2, 1) is not None \
+                and self._board.tile_at(2, 2).Symbol is not None:
             # if first row is full with same symbol
             if self._board.tile_at(2, 0).Symbol == self._board.tile_at(2, 1).Symbol \
                     and self._board.tile_at(2, 2).Symbol == self._board.tile_at(2, 1).Symbol:
