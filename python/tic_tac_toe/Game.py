2a1c2019cd77d67cd817b62b31c95b8bc0244229
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-03-16 16:08:25 +0100

    refactor: remove comments from the game

--- python/tic_tac_toe/Game.py
+++ python/tic_tac_toe/Game.py
@@ -8,7 +8,6 @@ class Game(object):
         self._board = Board()
 
     def play(self, symbol, x, y):
-        # if first move
         if self._last_player is None and symbol == 'O':
             raise Exception('Invalid first player')
 
@@ -18,7 +17,6 @@ class Game(object):
         if self._board.tile_at(x, y) is not None:
             raise Exception('Invalid position')
 
-        # update game state
         self._last_player = symbol
         self._board.add_tile_at(symbol, x, y)
 
@@ -27,7 +25,6 @@ class Game(object):
                 and self._board.tile_at(row, 2) == self._board.tile_at(row, 1)
 
     def winner(self):
-        # if the positions in first row are taken
         for row in range(3):
             if self._board.tile_at(row, 0) is not None \
                     and self._board.tile_at(row, 1) is not None \
